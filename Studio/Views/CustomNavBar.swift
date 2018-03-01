70b6b1a8774590eeedb95c403d31f7c496b64216
ttionn <matt.tian@icloud.com>
2018-03-01 14:10:59 +0800

    Update nav bar tint color

--- Studio/Views/CustomNavBar.swift
+++ Studio/Views/CustomNavBar.swift
@@ -35,6 +35,7 @@ class CustomNavBar: UIViewController {
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.tintColor = .white
         
         let titleAttributes = [NSAttributedStringKey.foregroundColor: UIColor.black]
         navigationController?.navigationBar.titleTextAttributes = titleAttributes
@@ -80,12 +81,28 @@ class WallpaperView2: UIViewController {
         return "landscape2"
     }
     
+    lazy var footprint: UIButton = {
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "footprint"), for: .normal)
+        button.tintColor = UIColor.fromHEX(string: "#33506A")
+        button.addTarget(self, action: #selector(handleFootprint), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         navigationItem.title = "Alone"
         setupViews()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 2, delay: 2, options: [.allowUserInteraction, .autoreverse, .repeat, .curveEaseIn], animations: {
+            self.footprint.alpha = 0.4
+        })
     }
     
     private func setupViews() {
@@ -96,10 +113,48 @@ class WallpaperView2: UIViewController {
         view.addSubview(imageView)
         view.addConstraints(format: "H:|[v0]|", views: imageView)
         view.addConstraints(format: "V:|[v0]|", views: imageView)
+        
+        view.addSubview(footprint)
+        view.addConstraints(format: "H:[v0(32)]", views: footprint)
+        view.addConstraints(format: "V:[v0(32)]-40-|", views: footprint)
+        footprint.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    @objc func handleFootprint() {
+        navigationController?.pushViewController(WallpaperView3(), animated: true)
     }
     
 }
 
 class WallpaperView3: WallpaperView2 {
     override var imageName: String { return "landscape3" }
+    
+    override lazy var footprint: UIButton = {
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "footprint"), for: .normal)
+        button.tintColor = UIColor.fromHEX(string: "#B95C76")
+        button.addTarget(self, action: #selector(handleFootprint), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc override func handleFootprint() {
+        navigationController?.pushViewController(WallpaperView4(), animated: true)
+    }
+}
+
+class WallpaperView4: WallpaperView2 {
+    override var imageName: String { return "landscape4" }
+    
+    override lazy var footprint: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Sunset"
+        let titleAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = titleAttributes
+    }
 }
